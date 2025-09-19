45a2db03c769f2642b32d3ee437791e29f404245
SirAlex36 <alexngenoo@gmail.com>
2025-09-19 16:08:39 +0300

    initial commit

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeButton = document.getElementById("like");
+const errorModal = document.getElementById("error-modal");
+const errorMessage = document.getElementById("error-message");
 
 
 
@@ -23,3 +26,25 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+likeButton.addEventListener("click", () => {
+  mimicServerCall()
+    .then(() => {
+      if (likeButton.textContent === EMPTY_HEART) {
+        likeButton.textContent = FULL_HEART;
+        likeButton.classList.add("activated-heart");
+      } else {
+        likeButton.textContent = EMPTY_HEART;
+        likeButton.classList.remove("activated-heart");
+      }
+    })
+    .catch((error) => {
+      // Show modal
+      errorMessage.textContent = error;
+      errorModal.classList.remove("hidden");
+
+      // Hide after 3s
+      setTimeout(() => {
+        errorModal.classList.add("hidden");
+      }, 3000);
+    });
+});
